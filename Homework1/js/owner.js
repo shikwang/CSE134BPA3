5d34f1f49e17b716a74b5583c40e15763f817324
Xinghang Li <xil248@ucsd.edu>
2018-02-17 11:53:03 -0800

    d

--- Homework1/js/owner.js
+++ Homework1/js/owner.js
@@ -3,10 +3,12 @@ window.onload = function(){
 
     
     var modal = document.getElementById('myModal');
+    var updateModal = document.getElementById('updateModal');
     // Get the button that opens the modal
     var btn = document.getElementById("myBtn");
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
+    var span2 = document.getElementsByClassName("close")[1];
 
     // When the user clicks the button, open the modal 
     document.getElementById("myBtn").onclick = function() {
@@ -16,6 +18,11 @@ window.onload = function(){
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
         modal.style.display = "none";
+        // updateModal.style.display = "none";
+    }
+    span2.onclick = function() {
+        // modal.style.display = "none";
+        updateModal.style.display = "none";
     }
 
     // When the user clicks anywhere outside of the modal, close it
@@ -23,6 +30,10 @@ window.onload = function(){
         if (event.target == modal) {
             modal.style.display = "none";
         }
+        if (event.target == updateModal) {
+            updateModal.style.display = "none";
+        }
+
     }
 
 
@@ -61,7 +72,9 @@ window.onload = function(){
                         // '<h3> Toasted muffin with smoked bacon and two poached eggs topped with hollandaise sauce and Sriracha </h3>'+
                         '<h4>$' + price + '</h4>'+
                         // '<button onclick="deleteMenu()"> Delete </button>';
-                        '<button onclick="deleteMenu(' + curID.toString() + ')"> Delete </button>';
+                        '<button class="btn btn-danger" onclick="deleteMenu(' + curID.toString() + ')"> Delete </button>'+
+                        '<button class="btn btn-warning" onclick="updateMenu(' + curID.toString() + ')"> Update </button>';;
+                        // '<button onclick="deleteMenu(' + curID.toString() + ')"> Delete </button>'
         document.body.appendChild(div);  
         idCount ++;
         modal.style.display = "none";
@@ -117,12 +130,14 @@ function loadMenu() {
             div.id = curID;
 
             div.className = 'floating-box';
-            div.innerHTML = '<img src="./images/' + imgPath + '" alt="IMAGE" style="width:200px;height:160px;margin-top:25px">' +
+            div.innerHTML = '<img id="Img" src="./images/' + imgPath + '" alt="IMAGE" style="width:200px;height:160px;margin-top:25px">' +
                 '<h2>' + name + '</h2>' +
                 // '<h3> Toasted muffin with smoked bacon and two poached eggs topped with hollandaise sauce and Sriracha </h3>'+
                 '<h4>$' + price + '</h4>' +
                 // '<button onclick="deleteMenu()"> Delete </button>';
-                '<button onclick="deleteMenu(' + curID.toString() + ')"> Delete </button>';
+                '<button class="btn btn-danger" onclick="deleteMenu(' + curID.toString() + ')"> Delete </button>' + 
+                '<button class="btn btn-warning" onclick="updateMenu(' + curID.toString() + ')"> Update </button>';
+                // '<button id="myBtn"> Update </button>';
             document.body.appendChild(div);
             // idCount ++;
             // modal.style.display = "none";
@@ -147,6 +162,54 @@ function deleteMenu( menuID ){
     // loadMenu();
 
 }
+
+function updateMenu( menuID){
+    // console.log("!!!!!!!!!!!");
+    // console.log(JSON.parse(localStorage.menuList)[menuID['id']]);
+    console.log(document.getElementsByClassName("close"))
+
+    // console.log(menuID['id']);
+    var id = menuID['id'];
+    var curItem = JSON.parse(localStorage.menuList)[menuID['id']];
+    document.getElementById('upName').value = curItem['Name']; 
+    document.getElementById('upPrice').value = curItem['Price']; 
+    document.getElementById('upImg').value = curItem['ImgPath']; 
+    console.log(id);
+    localStorage.setItem("updateID",id);
+    // localStorage.setItem("idCounter", idCount);
+    document.getElementById('updateModal').style.display = "block";
+
+}
+
+function updateMenuOK (){
+    var y = document.getElementById("updateMenuForm");
+    var name = y.elements[0].value;
+    var price = y.elements[1].value;
+    var imgPath = y.elements[2].value;
+    // console.log(prie);
+
+    var curID = localStorage.getItem("updateID");
+    // console.log(localStorage.getItem("updateID"));
+    // localStorage.removeItem('menuList');
+    if(localStorage.menuList){
+        var menuList = JSON.parse(localStorage.menuList);
+        menuList[curID] = {Name:name,Price:price,ImgPath:imgPath};
+        localStorage.setItem("menuList",JSON.stringify(menuList));
+        // console.log(JSON.parse(localStorage.menuList));
+    }
+    else{ 
+        var curMenu = {};
+        curMenu[curID] = {Name:name,Price:price,ImgPath:imgPath};
+        localStorage.setItem("menuList",JSON.stringify(curMenu));
+    }
+
+    location.href = "MenuOwner.html";
+
+
+
+}
+
+
 
 
    
